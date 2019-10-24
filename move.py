9036a98039a2093c9ea58bb3c217e07d025104c3
josh-flori <joshuaflori@gmail.com>
2019-10-23 21:30:41 -0500

    Update move.py

--- move.py
+++ move.py
@@ -22,10 +22,19 @@ events['cumsum']=events['NUM_VISITOR_GROUPS'].cumsum()
 
 
 img = Image.new('RGB', (1920,1080), color = (26, 26, 26))
+
+
+
 # set font
 fnt = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 30)
 
 d = ImageDraw.Draw(img)
+
+d.rectangle(((100, 980), (1880, 100)))
+d.rectangle(((100, 100), (1880, 100)),outline=(26, 26, 26))
+d.rectangle(((1880, 980), (1880, 100)),outline=(26, 26, 26))
+
+
 # add user
 n=4
 x=events['TIME'][0]

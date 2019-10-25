a7b1dd1ad2dcaa7b45420056c1652cf10e78c546
josh-flori <joshuaflori@gmail.com>
2019-10-24 23:05:33 -0500

    Update move.py

--- move.py
+++ move.py
@@ -1,6 +1,22 @@
-# /users/josh.flori/my/bin/python /users/josh.flori/documents/github/file_mover/move.py
+# Cumulative trick-or-treater visits 
+
+
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
+
+
+
+
+import wave
+from os import path
+from pydub import AudioSegment
+from pydub import AudioSegment
+from pydub.playback import play
+from moviepy.editor import *
+import numpy as np
+import itertools
+
+
 
 
 
@@ -28,15 +44,17 @@ img = Image.new('RGB', (1920,1080), color = (26, 26, 26))
 # set font
 fnt1 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 40)
 fnt2 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 35)
+fnt3 = ImageFont.truetype('/Library/Fonts/Verdana.ttf', 25)
+
 
 d = ImageDraw.Draw(img)
 
 
 x_start=100
-x_end=1880
+x_end=1800
 # these are inverse of what you expect
 y_start=980
-y_end=100
+y_end=140
 
 d.rectangle(((x_start, y_start), (x_end, y_end)))
 d.rectangle(((x_start, y_end), (x_end, y_end)),outline=(26, 26, 26))
@@ -46,12 +64,10 @@ d.rectangle(((x_end, y_start), (x_end, y_end)),outline=(26, 26, 26))
 
 
 # some measurements
-n=4
 t=events['TIME'][0]
 n=events.shape[0]
 length=x_end-x_start-10 # 10 is visual padding
 height=y_start-y_end
-ticks=[x_start+5]*num_tens
 distance_per_tick=length/n
 vertical_axis=list(range(events['cumsum'][n-1]+1))
 vert_per_tick=height/vertical_axis[-1]
@@ -60,17 +76,26 @@ x="|"
 
 
 pumpkin = Image.open("/users/josh.flori/desktop/pumpkin.png")
+title=Image.open('/users/josh.flori/desktop/title.png')
 
 
 
 d.text((x_start+10,y_start-9), x, font=fnt1, fill=(255, 92, 57)) # 5 is padding, 9 is alignment
 d.text((x_start-15,y_start+40), events['TIME'][0][0:4], font=fnt2, fill=(255, 92, 57))
 
+# AXIS LABELS
+d.text((x_start-70,22), "Total Trick\n   - or - \n Treaters", font=fnt3, fill=(200, 200, 200))
+d.text((1840,1020), "PM", font=fnt2, fill=(200, 200, 200))
+
+
 for i in range(0,len(vertical_axis),5):
     q=45 if i<10 else 65
-    d.text((x_start-q,y_start-(vert_per_tick*i)-50), str(vertical_axis[i]), font=fnt1, fill=(255, 92, 57))
+    d.text((x_start-q,y_start-(vert_per_tick*i)-65), str(vertical_axis[i]), font=fnt1, fill=(255, 92, 57))
 
 img.save(directory+'/0.png')
+composite=Image.open(directory+'/0.png')
+composite.paste(title, (600, 10), title)
+composite.save(directory+'/0.png')
 
 
 for i in range(1,n):
@@ -78,16 +103,20 @@ for i in range(1,n):
     d = ImageDraw.Draw(composite)
     if i%10==0:
         d.text((x_start+10+(i*distance_per_tick),y_start-9), x, font=fnt1, fill=(255, 92, 57)) # 5 is padding, 9 is alignment
-        d.text((x_start-15+(i*distance_per_tick),y_start+40), events['TIME'][i][0:4], font=fnt2, fill=(255, 92, 57))
+        d.text((x_start-17+(i*distance_per_tick),y_start+40), events['TIME'][i][0:4], font=fnt2, fill=(255, 92, 57))
         composite.save(directory+'/'+str(i)+'.png')
     else:
         d.text((x_start+10+(i*distance_per_tick),y_start-11), ".", font=fnt1, fill=(255, 92, 57))
         if events['cumsum'][i-1] != events['cumsum'][i]:
-            composite.paste(pumpkin, (x_start+10+(i*distance_per_tick), y_start-50-(int(vert_per_tick)*events['cumsum'][i])), pumpkin)
+            composite.paste(pumpkin, (x_start+(i*int(distance_per_tick)), y_start-60-(int(vert_per_tick)*events['cumsum'][i])), pumpkin)
             composite.save(directory+'/'+str(i)+'.png')
         else:
             composite.save(directory+'/'+str(i)+'.png')
 
     
 
+clips = [ImageClip(directory+'/'+str(i)+".png").set_duration(.05) if i != n-1 else ImageClip(directory+'/'+str(i)+".png").set_duration(1.3) for i in range(n)] # <-- give the last frame a little time to breath before jumping into the next 
 
+
+concat_clip = concatenate_videoclips(clips, method="compose")
+concat_clip.write_videofile(directory+"/spooky.mp4", fps=30)
